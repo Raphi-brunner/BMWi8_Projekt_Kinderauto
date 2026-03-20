e4e59da85d9fcf00d3f65385419b6659c5299e43
codi688 <maximilian07bertl@gmail.com>
2026-03-20 13:28:06 +0100

    Motorsteuerung v1.0

---
+++ src/defines.h
@@ -0,0 +1,14 @@
+#ifndef DEFINES_H
+#define DEFINES_H
+
+#define TRIG_PIN 5
+#define ECHO_PIN_1 9
+#define ECHO_PIN_2 10
+#define Echo_PIN_3 11
+#define Echo_PIN_4 12 
+#define Echo_PIN_5 13
+#define Echo_PIN_6 14
+#define BUZZER 4 
+
+
+#endif
