135776f66206f51ac58472457d578f9e65f85b28
codi688 <maximilian07bertl@gmail.com>
2026-03-27 13:22:52 +0100

    update defines

--- src/defines.h
+++ src/defines.h
@@ -1,6 +1,7 @@
 #ifndef DEFINES_H
 #define DEFINES_H
 
+// JSN-SR04 Pins
 #define TRIG_PIN 5
 #define ECHO_PIN_1 9
 #define ECHO_PIN_2 10
@@ -8,7 +9,22 @@
 #define Echo_PIN_4 12 
 #define Echo_PIN_5 13
 #define Echo_PIN_6 14
+
+// Buzzer Pin
 #define BUZZER 4 
+
+// Motorsteuerung Pins
+#define AHI1 35
+#define ALI1 36
+#define BHI1 37
+#define BLI1 38
+#define DIS1 45
+
+#define AHI2 39
+#define ALI2 40
+#define BHI2 41
+#define BLI2 42
+#define DIS2 45
 
 
 #endif
