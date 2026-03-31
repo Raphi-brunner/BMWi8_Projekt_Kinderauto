4bdc61b606f059c72d31e0a06e9d75c54e2e0810
codi688 <maximilian07bertl@gmail.com>
2026-03-31 12:27:27 +0200

    changes

--- src/Motorsteuerung.cpp
+++ src/Motorsteuerung.cpp
@@ -10,8 +10,8 @@ void MotorDriver::begin() {
   pinMode(_DIS, OUTPUT);
 
   // PWM einrichten
-  ledcSetup(0, 5000, 8);    // Kanal 0, 5 kHz, 8 Bit
-  ledcSetup(1, 5000, 8);    // Kanal 1, 5 kHz, 8 Bit
+  ledcSetup(0, 20000, 8);    // Kanal 0, 20 kHz, 8 Bit
+  ledcSetup(1, 20000, 8);    // Kanal 1, 20 kHz, 8 Bit
 
   ledcAttachPin(_ALI, 0);   // PWM ALI -> PWM Kanal 0
   ledcAttachPin(_BLI, 1);   // PWM BLI -> PWM Kanal 1
@@ -23,26 +23,29 @@ void MotorDriver::forward(int speed) {
   // Stop vorher aktivieren, um Deadtime einzuhalten
   digitalWrite(_DIS, LOW); // Motoren freigeben
   stop();
-  delay(2);  // Deadtime: 2 ms, kann je nach H-Brücke angepasst werden
+  delayMicroseconds(2);  // Deadtime
 
   digitalWrite(_AHI, HIGH);
   digitalWrite(_BHI, LOW);
-
-  ledcWrite(0, speed); // PWM Kanal 0 (_ALI)
+  ledcWrite(0, 0); //anderen PWM-Kanal auf LOW setzen, um sicherzustellen, dass nur einer aktiv ist
+  ledcWrite(1, speed); // PWM Kanal 1 (_BLI) aktivieren
 }
 
 void MotorDriver::backward(int speed) {
   digitalWrite(_DIS, LOW); // Motoren freigeben
   stop();
-  delay(2);  // Deadtime
+  delayMicroseconds(2);  // Deadtime
 
   digitalWrite(_AHI, LOW);
   digitalWrite(_BHI, HIGH);
-
-  ledcWrite(1, constrain(speed, 0, 170)); // PWM Kanal 1 (_BLI) beschränkt auf 170 (ca. 66% Leistung) für Rückwärtsfahrt.
+  ledcWrite(1, 0); //anderen PWM-Kanal auf LOW setzen, um sicherzustellen, dass nur einer aktiv ist
+  ledcWrite(0, constrain(speed, 0, 170)); // PWM Kanal 0 (_ALI) beschränkt auf 170 (ca. 66% Leistung) für Rückwärtsfahrt.
 }
 
 void MotorDriver::stop() {
+  ledcWrite(0, 0);
+  ledcWrite(1, 0);
+
   digitalWrite(_AHI, LOW);
   digitalWrite(_ALI, LOW);
   digitalWrite(_BHI, LOW);
